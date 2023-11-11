d762da2c546a8eecd278c06438108acefdf3399f
Reece Yang <reeceyang@live.com>
2023-11-11 16:12:37 -0500

    footer links

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html data-theme="retro" lang="en">
-      <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🦈</text></svg>" />
-      </head>
       <body className={inter.className}>
         <Header />
         <div className="overflow-clip">{children}</div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,16 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { IconDefinition, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+
+const SOCIAL_LINKS: { url: string; icon: IconDefinition }[] = [
+  { url: "https://github.com/reeceyang", icon: faGithub },
+  {
+    url: "https://www.linkedin.com/in/reece-yang-52b16a189/",
+    icon: faLinkedin,
+  },
+  { url: "mailto:rya@mit.edu", icon: faEnvelope },
+];
 
 export default function Footer() {
   return (
@@ -18,11 +28,13 @@ export default function Footer() {
       </div>
       <div>
         <div className="grid grid-flow-col gap-4">
-          <a
-            className="link transition hover:scale-110"
-          >
-            <FontAwesomeIcon width="24" height="24" icon={faGithub} />
-          </a>
+          {SOCIAL_LINKS.map(({url, icon}) =>
+          <Link href={url} key={url}>
+            <FontAwesomeIcon
+              className="w-6 h-6 link transition hover:scale-110"
+              icon={icon}
+            />
+          </Link>)}
         </div>
       </div>
       <div>
