d26c58df87d46b13111ca56a4e40ddb8d963c912
Reece Yang <reeceyang@live.com>
2024-07-15 23:32:12 -0700

    feat: restyle header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
 
 const Header = () => (
-  <div className="navbar bg-white min-h-min mb-12">
-    <ul className="menu menu-md menu-horizontal max-w-4xl m-auto w-full p-0">
-      <li>
-        <Link href="/" className="font-bold">
-          Reece Yang
-        </Link>
-      </li>
-      <li>
-        <Link href="/projects">Projects</Link>
-      </li>
-      <li>
-        <Link href="/blog">Blog</Link>
-      </li>
-    </ul>
-  </div>
+  <>
+    <div className="navbar min-h-min mb-12 fixed glass">
+      <ul className="menu menu-md menu-horizontal max-w-4xl m-auto w-full p-0">
+        <li>
+          <Link href="/" className="font-bold">
+            Reece Yang
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+      </ul>
+    </div>
+    <div className="h-24" />
+  </>
 );
 
 export default Header;
