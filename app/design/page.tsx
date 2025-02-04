9096c069e7856a9732ca9f4bb6fd194156634b59
Reece Yang <reeceyang@live.com>
2025-02-04 11:49:28 -0500

    add lates to design page only

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -11,22 +11,32 @@ const DESIGN_PROJECTS = [
   "convoscope",
   "henry",
   "mpc",
+  "lates",
 ];
+
+const latesProject = {
+  url: "https://lates.vercel.app",
+  image_src: "/lates.png",
+  description: "Plan menus and ordering food for a co-op meal plan.",
+  title: "Lates",
+  slug: "lates",
+  categories: ["TypeScript", "React"],
+};
 
 export default async function Projects() {
   return (
     <>
       <h1 className="text-5xl font-bold mb-4">Design Projects</h1>
       <div className="flex flex-col py-6 gap-24">
-        {PROJECTS.filter(({ slug }) => DESIGN_PROJECTS.includes(slug)).map(
-          (project, i) => (
+        {[...PROJECTS, latesProject]
+          .filter(({ slug }) => DESIGN_PROJECTS.includes(slug))
+          .map((project, i) => (
             <ProjectCard
               project={project}
               key={project.slug}
               divider={i < PROJECTS.length - 1}
             />
-          ),
-        )}
+          ))}
       </div>
     </>
   );
